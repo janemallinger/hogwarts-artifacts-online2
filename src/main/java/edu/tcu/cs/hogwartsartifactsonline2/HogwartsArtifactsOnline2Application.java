eab266e19a458d78c4e77969717e636ae8b1e90b
Jane Allinger <janeallinger@Janes-Air.lan>
2025-03-25 00:36:41 -0500

    First commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline2/HogwartsArtifactsOnline2Application.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnline2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactsOnline2Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline2/HogwartsArtifactsOnline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnline2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
