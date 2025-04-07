c6747c2561eac6729f7b4cd7dff6e5de9deafa94
Jane Allinger <janeallinger@Janes-MacBook-Air.local>
2025-04-07 13:43:46 -0500

    id worker update

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline2/HogwartsArtifactsOnline2Application.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline2/HogwartsArtifactsOnline2Application.java
@@ -1,13 +1,20 @@
 package edu.tcu.cs.hogwartsartifactsonline2;
 
+import edu.tcu.cs.hogwartsartifactsonline2.artifact.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HogwartsArtifactsOnline2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(HogwartsArtifactsOnline2Application.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1, 1);
     }
 
 }
